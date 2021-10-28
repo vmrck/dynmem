718a3708e85caa8ff705ee141004e9d87cc47369
vmrck <91593338+vmrck@users.noreply.github.com>
2021-10-28 10:51:04 +0200

    Added dynamic Stack and Queue

---
+++ dynqueue.h
@@ -0,0 +1,48 @@
+using namespace std;
+
+template <typename T>
+struct node {
+  T data;
+  node* next;
+};
+
+template <typename T>
+struct queue {
+  node<T>* head;
+  node<T>* tail;
+};
+
+template <typename T>
+void init(queue<T>& q) {
+  q.head = NULL;
+  q.tail = NULL;
+}
+
+template <typename T>
+bool empty(queue<T> q) {
+  return q.head == NULL;
+}
+
+template <typename T>
+void enqueue(queue<T>& q, T value) {
+  node<T>* aux = new(node<T>);
+  aux->data = value;
+  aux->next = NULL;
+  if(q.head == NULL) {
+    q.head = aux;
+    q.tail = aux;
+  } else {
+    q.tail->next = aux;
+    q.tail = aux;
+  }
+}
+
+template <typename T>
+void dequeue(queue<T>& q) {
+  q.head = q.head->next;
+}
+
+template <typename T>
+T front(queue<T> q) {
+  return q.head->data;
+}
