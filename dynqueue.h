784f4c876efd8bb2e088062ccc8f52ad70ee633a
vmrck <91593338+vmrck@users.noreply.github.com>
2021-10-28 11:51:09 +0200

    Updated with namespaces

--- dynqueue.h
+++ dynqueue.h
@@ -1,48 +1,64 @@
 using namespace std;
 
-template <typename T>
-struct node {
-  T data;
-  node* next;
-};
-
-template <typename T>
-struct queue {
-  node<T>* head;
-  node<T>* tail;
-};
-
-template <typename T>
-void init(queue<T>& q) {
-  q.head = NULL;
-  q.tail = NULL;
-}
-
-template <typename T>
-bool empty(queue<T> q) {
-  return q.head == NULL;
-}
-
-template <typename T>
-void enqueue(queue<T>& q, T value) {
-  node<T>* aux = new(node<T>);
-  aux->data = value;
-  aux->next = NULL;
-  if(q.head == NULL) {
-    q.head = aux;
-    q.tail = aux;
-  } else {
-    q.tail->next = aux;
-    q.tail = aux;
+namespace dynqueue {
+  template <typename T>
+  struct node {
+    T data;
+    node* next;
+  };
+  
+  template <typename T>
+  struct queue {
+    node<T>* head;
+    node<T>* tail;
+  };
+  
+  template <typename T>
+  void init(queue<T>& q) {
+    q.head = NULL;
+    q.tail = NULL;
   }
-}
-
-template <typename T>
-void dequeue(queue<T>& q) {
-  q.head = q.head->next;
-}
-
-template <typename T>
-T front(queue<T> q) {
-  return q.head->data;
+  
+  template <typename T>
+  bool empty(queue<T> q) {
+    return q.head == NULL;
+  }
+  
+  template <typename T>
+  void enqueue(queue<T>& q, T value) {
+    node<T>* aux = new(node<T>);
+    aux->data = value;
+    aux->next = NULL;
+    if(q.head == NULL) {
+      q.head = aux;
+      q.tail = aux;
+    } else {
+      q.tail->next = aux;
+      q.tail = aux;
+    }
+  }
+  
+  template <typename T>
+  void dequeue(queue<T>& q) {
+    q.head = q.head->next;
+  }
+  
+  template <typename T>
+  T front(queue<T> q) {
+    return q.head->data;
+  }
+  
+  template <typename T>
+  void prt(queue<T> q) {
+    cout << "\nElementi della coda: ";
+    prtHelper(q.head);
+  }
+  
+  template <typename T>
+  void prtHelper(node<T>* n) {
+    if(n != NULL) {
+      cout << n->data << " ";
+      prtHelper(n->next);
+    }
+  }
 }
--- dynstack.h
+++ dynstack.h
@@ -1,40 +1,56 @@
 using namespace std;
 
-template <typename T>
-struct node {
-  T data;
-  node* next;
-};
-
-template <typename T>
-struct stack {
-  node<T>* head;
-};
-
-template <typename T>
-void init(stack<T>& s) {
-  s.head = NULL;
-}
-
-template <typename T>
-bool empty(stack<T> s) {
-  return s.head == NULL;
-}
-
-template <typename T>
-void push(stack<T>& s, T value) {
-  node<T>* aux = new(node<T>);
-  aux->data = value;
-  aux->next = s.head;
-  s.head = aux;
-}
-
-template <typename T>
-void pop(stack<T>& s) {
-  s.head = s.head->next;
-}
-
-template <typename T>
-T top(stack<T> s) {
-  return s.head->data;
+namespace dynstack {
+  template <typename T>
+  struct node {
+    T data;
+    node* next;
+  };
+  
+  template <typename T>
+  struct stack {
+    node<T>* head;
+  };
+  
+  template <typename T>
+  void init(stack<T>& s) {
+    s.head = NULL;
+  }
+  
+  template <typename T>
+  bool empty(stack<T> s) {
+    return s.head == NULL;
+  }
+  
+  template <typename T>
+  void push(stack<T>& s, T value) {
+    node<T>* aux = new(node<T>);
+    aux->data = value;
+    aux->next = s.head;
+    s.head = aux;
+  }
+  
+  template <typename T>
+  void pop(stack<T>& s) {
+    s.head = s.head->next;
+  }
+  
+  template <typename T>
+  T top(stack<T> s) {
+    return s.head->data;
+  }
+  
+  template <typename T>
+  void prt(stack<T> s) {
+    cout << "\nElementi dello stack: ";
+    prtHelper(s.head);
+  }
+  
+  template <typename T>
+  void prtHelper(node<T>* n) {
+    if(n != NULL) {
+      cout << n->data << " ";
+      prtHelper(n->next);
+    }
+  }
 }
